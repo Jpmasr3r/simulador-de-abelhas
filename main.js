daa67d0921c124c7ca4eb3c1e4817a0f4c5386c1
Jpmasr3r <jpanjolimsoares@gmail.com>
2025-07-28 21:52:03 -0300

    commit

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ beeImg.src = 'assets/bee.png';
 queenImg.src = 'assets/queen.png';
 
 const abelhas = [];
+let queen;
+let queenWay = [];
 
 function spawnAbelhas() {
     for (let i = 0; i < 10; i++) {
@@ -21,27 +23,55 @@ function spawnAbelhas() {
         });
     }
 
-    abelhas.push({
+    for (i = 0; i <= 4; i++) {
+        let way = {
+            x: Math.random() * (canvas.width - 64),
+            y: Math.random() * (canvas.height - 64),
+        }
+        queenWay.push(way);
+    }
+
+    queen = {
         tipo: 'rainha',
-        x: Math.random() * (canvas.width - 32),
-        y: Math.random() * (canvas.height - 32),
+        x: Math.random() * (canvas.width - 64),
+        y: Math.random() * (canvas.height - 64),
         img: queenImg
-    });
+    }
 }
 
 function desenhar() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     for (const abelha of abelhas) {
-        ctx.drawImage(abelha.img, Math.floor(abelha.x), Math.floor(abelha.y), 32, 32);
+        switch (abelha.tipo) {
+            case "operaria":
+                ctx.drawImage(abelha.img, Math.floor(abelha.x), Math.floor(abelha.y), 32, 32);
+                break;
+
+            default:
+                break;
+        }
     }
 
-    requestAnimationFrame(desenhar);
+    ctx.drawImage(queen.img, Math.floor(queen.x), Math.floor(queen.y), 64, 64);
 }
+
+function move() {
+    const spd = 1;
+    queenWay.forEach(way => {
+        queen.x += spd * Math.sign(way.x);
+        queen.y += spd * Math.sign(way.y);
+    });
+}
+
+function loop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    desenhar();
+    move();
+    requestAnimationFrame(loop);
+};
 
 beeImg.onload = () => {
     queenImg.onload = () => {
         spawnAbelhas();
-        desenhar();
+        loop();
     };
 };
